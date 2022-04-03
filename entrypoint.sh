dccf2721cb4cace1a2a19b4f30b8826428d68f7b
Cristian Magadan <cristian@magadan.com.br>
2022-04-03 14:17:06 -0300

    .
    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,16 @@
 if [ -z $MODE ]; then
   MODE=server
 fi
+
 if [ $MODE = server ]; then
   if [ $BIND_PORT ]; then
     sed -i "s|bind_port = 7000|bind_port = $BIND_PORT|g" /frp/frps.ini
   fi
   if [ $DASHBOARD_PORT ]; then
-    sed -i "s|dashboard_port = 7500|dashboard_port = $DASHBOARD_PORT|g" /frp/frps.ini
+    sed "s|dashboard_port = 7500|dashboard_port = $DASHBOARD_PORT|g" /frp/frps.ini
   fi
   if [ $DASHBOARD_USER ]; then
-    sed -i "s|dashboard_user = 7500|dashboard_user = $DASHBOARD_USER|g" /frp/frps.ini
+    sed "s|dashboard_user = 7500|dashboard_user = $DASHBOARD_USER|g" /frp/frps.ini
   fi
   if [ $DASHBOARD_PWD ]; then
     sed -i "s|dashboard_user = 7500|dashboard_user = $DASHBOARD_PWD|g" /frp/frps.ini
